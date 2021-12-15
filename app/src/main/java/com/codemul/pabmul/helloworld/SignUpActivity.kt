e1493ec3bf8e8c79dcb9324aa3190a131d97bb16
ilham-2001 <arizqyakbar@gmail.com>
2021-12-15 20:19:08 +0700

    add sign up stuff

--- app/src/main/java/com/codemul/pabmul/helloworld/SignUpActivity.kt
+++ app/src/main/java/com/codemul/pabmul/helloworld/SignUpActivity.kt
@@ -3,28 +3,56 @@ package com.codemul.pabmul.helloworld
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.codemul.pabmul.helloworld.databinding.ActivityMainBinding
+import com.codemul.pabmul.helloworld.databinding.ActivitySignUpBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setViewListener()
     }
 
-    fun setViewListener(){
-        val signUpBtn: Button = findViewById(R.id.btn_signup)
+
+    private fun setViewListener() {
         val tvPunyaAkun: TextView = findViewById(R.id.tv_punya_akun_signup)
 
-        signUpBtn.setOnClickListener {
-            val signUpIntent = Intent(this@SignUpActivity, LoginActivity::class.java)
-            startActivity(signUpIntent)
-        }
-            tvPunyaAkun.setOnClickListener {
+
+        tvPunyaAkun.setOnClickListener {
             val punyaAkunIntent = Intent(this@SignUpActivity, LoginActivity::class.java)
             startActivity(punyaAkunIntent)
         }
+
+        submitActionCreateAccount()
+    }
+
+    private fun submitActionCreateAccount() {
+        binding.btnSignup.setOnClickListener {
+
+            if (binding.etPasswordSignup.text.toString() == binding.etPasswordConfirmSignup.text.toString())
+            {
+                    // bagian masukkan data ke firebase di sini
+
+                    val signUpIntent = Intent(this@SignUpActivity, LoginActivity::class.java)
+                    startActivity(signUpIntent)
+            }
+
+            else{
+                Toast.makeText(this@SignUpActivity, "Password do not match", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 }
