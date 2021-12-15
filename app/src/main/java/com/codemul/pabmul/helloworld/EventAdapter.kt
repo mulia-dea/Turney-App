28e464a1feaacee7cfaf42819661ca7f564ec02e
Mul101 <mulia4404@gmail.com>
2021-12-16 00:29:38 +0700

    revisi

--- app/src/main/java/com/codemul/pabmul/helloworld/EventAdapter.kt
+++ app/src/main/java/com/codemul/pabmul/helloworld/EventAdapter.kt
@@ -11,9 +11,14 @@ import com.codemul.pabmul.helloworld.data.Event
 import com.squareup.picasso.Picasso
 
 class EventAdapter(private val context: Context, private val listEvent: List<Event>) : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
+    private var onItemClickCallback: OnItemClickCallback? = null
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
 
-
-
+    interface OnItemClickCallback {
+        fun onItemClicked(event: Event)
+    }
 //    private val eventList=  ArrayList<DataEvent>()
 //    var listEvents = ArrayList<DataEvent>()
 //    set(listEvents) {
@@ -36,14 +41,14 @@ class EventAdapter(private val context: Context, private val listEvent: List<Eve
     }
 
     override fun onBindViewHolder(holder: EventAdapter.EventViewHolder, position: Int) {
-//        holder.bind(listEvent[position])
-        val listEvent = listEvent[position]
-        holder.tvName.text = listEvent.name
-        holder.tvTglEvent.text = listEvent.tgl_event
-
-        var imageUri : String?= null
-        imageUri = listEvent.image
-        Picasso.get().load(imageUri).into(holder.image)
+        holder.bind(listEvent[position])
+//        val listEvent = listEvent[position]
+//        holder.tvName.text = listEvent.name
+//        holder.tvTglEvent.text = listEvent.tgl_event
+//
+//        var imageUri : String?= null
+//        imageUri = listEvent.image
+//        Picasso.get().load(imageUri).into(holder.image)
 //        Glide.with(holder.itemView.context)
 //            .load(listEvent.image)
 //            .apply(RequestOptions().override(200, 100))
@@ -59,18 +64,22 @@ class EventAdapter(private val context: Context, private val listEvent: List<Eve
         var tvTglEvent : TextView = itemView.findViewById(R.id.tv_tgl_event)
         var image : ImageView = itemView.findViewById(R.id.img_list_event)
 
-//        fun bind(event: Event) {
-//            tvName.text = event.name
-//            tvTglEvent.text = event.tgl_event
-//
-////            val option = RequestOptions().placeholder(R.mipmap.ic_launcher)
-////                .error(R.mipmap.ic_launcher)
+        var imageUri : String?= null
+        fun bind(event: Event) {
+            tvName.text = event.name
+            tvTglEvent.text = event.tgl_event
+            imageUri = event.image
+            Picasso.get().load(imageUri).into(image)
+            itemView.setOnClickListener{ onItemClickCallback?.onItemClicked(event)}
+
+//            val option = RequestOptions().placeholder(R.mipmap.ic_launcher)
+//                .error(R.mipmap.ic_launcher)
 //            Glide.with(itemView.context)
 //                .load(event.image)
 //                .apply(RequestOptions().override(200, 100))
 //                .into(image)
-//
-//        }
+
+        }
 
 //            if (event.image != null) {
 //                image.setImageBitmap(BitmapFactory.decodeFile(event.image))
