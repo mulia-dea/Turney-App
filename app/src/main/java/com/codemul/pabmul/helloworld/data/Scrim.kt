df0d74de2707806548353f57736ac7919149055a
ilham-2001 <arizqyakbar@gmail.com>
2021-12-15 21:37:10 +0700

    initial commit

--- app/src/main/java/com/codemul/pabmul/helloworld/data/Scrim.kt
+++ app/src/main/java/com/codemul/pabmul/helloworld/data/Scrim.kt
@@ -9,7 +9,6 @@ data class Scrim(
     var id: Int = 0,
     var nama_penyelenggara: String? = null,
     var tgl_pelaksanaan_scrim: String? = null,
-//    var jenis_game: JenisGame? = null,
     var jenis_game: String? = null,
     var jumlah_pemain: Int = 0,
     var informasi_scrim: String? = null,
