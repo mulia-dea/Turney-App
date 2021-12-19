2ee5e805b8b9ae5b3a7fc8f3787b1b86f177305c
ilham-2001 <arizqyakbar@gmail.com>
2021-12-20 04:30:23 +0700

    refine listener on scrim

--- app/src/main/java/com/codemul/pabmul/helloworld/ScrimAdapter.kt
+++ app/src/main/java/com/codemul/pabmul/helloworld/ScrimAdapter.kt
@@ -1,5 +1,6 @@
 package com.codemul.pabmul.helloworld
 
+import android.annotation.SuppressLint
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.codemul.pabmul.helloworld.data.Scrim
+import com.codemul.pabmul.helloworld.databinding.ListScrimBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -17,12 +19,6 @@ class ScrimAdapter(private val content: MutableList<Scrim>) :
 
     private lateinit var buttonListener: OnButtonJoinListener
 
-
-    interface OnButtonJoinListener {
-        fun onButtonJoinClick(content: Scrim)
-        fun onButtonUnjoinClick(content: Scrim)
-    }
-
     fun setOnClickButton(listener: OnButtonJoinListener) {
         buttonListener = listener
     }
@@ -30,81 +26,66 @@ class ScrimAdapter(private val content: MutableList<Scrim>) :
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
-    ): ScrimAdapter.ScrimViewHolder {
-        return ScrimViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.list_scrim, parent, false),
-            buttonListener
+    ): ScrimAdapter.ScrimViewHolder = ScrimViewHolder(
+        ListScrimBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
         )
-    }
+    )
 
     override fun onBindViewHolder(holder: ScrimAdapter.ScrimViewHolder, position: Int) {
-        holder.bind(position)
+        holder.bind(content[position])
     }
 
     override fun getItemCount(): Int {
         return content.size
     }
 
-    inner class ScrimViewHolder(itemView: View, buttonListener: OnButtonJoinListener) :
-        RecyclerView.ViewHolder(itemView) {
+    inner class ScrimViewHolder(val view: ListScrimBinding) :
+        RecyclerView.ViewHolder(view.root) {
 
-        var poster: ImageView
-        var playerCount: TextView
-        var gameType: TextView
-        var joinButton: Button
+        @SuppressLint("SetTextI18n")
+        fun bind(item: Scrim) {
+            when (item.jenis_game) {
+                "Valorant" -> view.imgScrim.setImageResource(R.drawable.valorant)
+                "PUBG" -> view.imgScrim.setImageResource(R.drawable.pubg_pic)
+                "DOTA 2" -> view.imgScrim.setImageResource(R.drawable.dota)
+                "Mobile Legends" -> view.imgScrim.setImageResource(R.drawable.mobilelegends)
+                else -> 0
+            }
 
+            view.tvPlayerCount.text = "${item.jumlah_pemain_sekarang}/${item.jumlah_pemain}"
+            view.tvNameGame.text = item.jenis_game
 
-        init {
-            poster = itemView.findViewById(R.id.img_scrim)
-            playerCount = itemView.findViewById(R.id.tv_player_count)
-            gameType = itemView.findViewById(R.id.tv_name_game)
-            joinButton = itemView.findViewById(R.id.button_join)
-
-//            var jumlahPemainSekarang = content[bindingAdapterPosition].jumlah_pemain_sekarang
-//            var jumlahPemain = content[bindingAdapterPosition].jumlah_pemain
-//            Log.d("index", bindingAdapterPosition.toString())
-
-            content.forEach {
-                if (it.jumlah_pemain_sekarang == it.jumlah_pemain) {
-                    joinButton.isEnabled = false
-                    //ganti background warna button
-                } else {
-                    joinButton.isEnabled = true
-                    if (it.isJoin == 0) {
-                        Log.d("Masuk if", "msk if boi")
-                        buttonListener.let {
-                            joinButton.setOnClickListener {
-                                buttonListener.onButtonJoinClick(content[bindingAdapterPosition])
-                                joinButton.setText("Unjoin")
-                            }
+            view.buttonJoin.setOnClickListener {
+                buttonListener.let {
+                    if (item.jumlah_pemain_sekarang != item.jumlah_pemain) {
+                        view.buttonJoin.isEnabled = true
+                        if (item.isJoin == 0) {
+                            Log.d("Masuk if", "msk if boi")
+                            item.isJoin = 1
+                            item.jumlah_pemain_sekarang += 1
+                            view.buttonJoin.setText("Unjoin")
+                            buttonListener.onButtonJoinClick(item, absoluteAdapterPosition)
+                        } else {
+                            item.jumlah_pemain_sekarang -= 1
+                            item.isJoin = 0
+                            Log.d("Masuk else", "msk else boi")
+                            view.buttonJoin.setText("Join")
+                            buttonListener.onButtonUnjoinClick(item, absoluteAdapterPosition)
                         }
-                        Log.d("join button", it.isJoin.toString())
-                    } else{
-                        Log.d("Masuk else", "msk else boi")
-                        buttonListener.let {
-                            joinButton.setOnClickListener {
-                                buttonListener.onButtonUnjoinClick((content[bindingAdapterPosition]))
-                                joinButton.setText("Join")
-                            }
-                        }
+
+                    } else {
+                        view.buttonJoin.isEnabled = false
                     }
                 }
             }
         }
+    }
 
 
-        internal fun bind(position: Int) {
-            when (content[position].jenis_game) {
-                "Valorant" -> poster.setImageResource(R.drawable.valorant)
-                "PUBG" -> poster.setImageResource(R.drawable.pubg_pic)
-                "DOTA 2" -> poster.setImageResource(R.drawable.dota)
-                "Mobile Legends" -> poster.setImageResource(R.drawable.mobilelegends)
-                else -> 0
-            }
-            playerCount.text =
-                "${content[position].jumlah_pemain_sekarang}/${content[position].jumlah_pemain}"
-            gameType.setText(content[position].jenis_game)
-        }
+    interface OnButtonJoinListener {
+        fun onButtonJoinClick(content: Scrim, position: Int)
+        fun onButtonUnjoinClick(content: Scrim, position: Int)
     }
 
 //    fun setData(item: List<Scrim>){
